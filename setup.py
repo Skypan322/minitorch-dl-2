431ec8fb3a44305f45341365970cbaaa49eed749
skovo <skovo@skovo.localdomain>
2026-09-18 21:51:59 +0300

    клоунское задание #5

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
 from setuptools import setup
 
 setup(packages=["minitorch"])
+
+# echo "Module 2"
+# pytest tests -x -m task2_1
+# pytest tests -x -m task2_2
+# pytest tests -x -m task2_3
+# pytest tests -x -m task2_4
+# echo "Module 3"
+# pytest tests -x -m task3_1
+# pytest tests -x -m task3_2
+# pytest tests -x -m task3_3
+# pytest tests -x -m task3_4
+# echo "Module 4"
+# pytest tests -x -m task4_1
+# pytest tests -x -m task4_2
+# pytest tests -x -m task4_3
+# pytest tests -x -m task4_4
